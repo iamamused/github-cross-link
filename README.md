afeb1439f654d45a5f5e3d474ea4c5b947009463
Jeffrey Sambells <github@tropicalpixels.com>
2010-10-21 23:51:15 -0400

    Added Chrome extension

--- README.md
+++ README.md
@@ -16,17 +16,19 @@ I'll add more possibilities as necessary.
 
 ##Installation
 
-1. [Download the packaged extension](http://github.com/downloads/iamamused/github-cross-link/github-cross-link.safariextz/qr_code)
+1. Download the packaged extension for [Safari](http://github.com/downloads/iamamused/github-cross-link/github-cross-link.safariextz/qr_code) or [Chrome](http://github.com/downloads/iamamused/github-cross-link/github-cross-link.crx/qr_code)
 
-2. If Safari doesn't prompt you to install, double click it.
+2. If your browser doesn't prompt you to install it you may need to double click it the file or drag it to your browser icon.
 
-3. To check if everyting is working, [go here](http://github.com/iamamused/github-cross-link/blob/master/test/Example.php) and see if you can click the Example_Class_Name.
+3. To check if everything is working, [go here](http://github.com/iamamused/github-cross-link/blob/master/test/Example.php) and see if you can click the Example_Class_Name.
 
-4. If that worked, go to Safari > Preferences > Extensions > GitHub Cross Link and enter the include patterns for your libraries. 
+4. If that worked you just need to add your own include patterns:
+  - In Safari go to: Safari > Preferences > Extensions > GitHub Cross-link 
+  - In Chrome go to: Window > Extensions > and select "Options" for GitHub Cross-link 
 
 ### Include Patterns
 
-For example, if you have a github project called `MyProject` your `My_Class_Name` class would have a url something like this:
+For example, if you have a GitHub project called `MyProject` your `My_Class_Name` class would have a url something like this:
 
     https://github.com/myusername/MyProject/blob/master/library/My/Class/Name.php
 
---
+++ github-cross-link.chromeextension/crosslink.js
@@ -0,0 +1,63 @@
+( function() {
+
+    function trim(str, chars) {
+        return ltrim(rtrim(str, chars), chars);
+    }
+     
+    function ltrim(str, chars) {
+        chars = chars || "\\s";
+        return str.replace(new RegExp("^[" + chars + "]+", "g"), "");
+    }
+     
+    function rtrim(str, chars) {
+        chars = chars || "\\s";
+        return str.replace(new RegExp("[" + chars + "]+$", "g"), "");
+    }
+
+    $(function() {
+    // The guts.
+    if (!localStorage["patterns"]) {
+      localStorage["patterns"] = '/iamamused/github-cross-link/*/test/php-library/';
+    };
+    
+    var url = window.location.href.split('/');
+    var base = url[0] + '/' + url[1] + '/' + url[2];
+    var user    = url[3];
+    var project = url[4];
+    var type    = url[5];
+    var version = url[6];
+    var extensionParts = url[url.length -1].split('.');
+    var extension = extensionParts[extensionParts.length -1];
+    
+    var patterns = localStorage["patterns"].split(',');
+    var found = false, pattern, parts;
+    for ( var i=0; i<patterns.length; i++ ) {
+        pattern = trim(patterns[i],'/');
+        parts = pattern.split('/');
+        if ( parts[1] === project ) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) return;
+    
+    var libraryBase = base + '/' + pattern.replace( '*', type + '/' + version ) + '/';
+    var classes = jQuery('.highlight .nx').each(function(i,e){
+        var node = $(e);
+        node.wrapInner(function() {
+            return $('<a/>').attr({
+                href : libraryBase + node.text().replace(/_/ig,'/') + '.' + extension,
+                title : 'View source in ' + project
+            }).css({
+                color: 'inherit'
+            });
+        });
+    });
+    
+  });
+
+  
+  // Done.
+  
+}() )
